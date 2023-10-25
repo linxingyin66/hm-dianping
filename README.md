2dc04473987f0155503569fbf84549378ea03526
LinXingYin <linxingyin_sky_66@163.com>
2023-10-25 20:29:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# hm-dianping
+
+#### 介绍
+黑马点评......
+
+#### 软件架构
+软件架构说明
+
+
+#### 安装教程
+
+1.  配置数据库地址，启动Mysql服务，导入sql脚本
+2.  配置Redis地址，启动Redis
+3.  启动Nginx
+4.  访问地址：[http://localhost:8080](http://)
+
+#### 使用说明
+
+1.  xxxx
+2.  xxxx
+3.  xxxx
---
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -0,0 +1,29 @@
+package com.hmdp.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.ShopType;
+import com.hmdp.mapper.ShopTypeMapper;
+import com.hmdp.service.IShopTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+
+    @Override
+    public Result queryTypeList() {
+        String sky = "cache:shopType:" ;
+        List<ShopType> list = query().orderByAsc("sort").list();
+        return null;
+    }
+}
